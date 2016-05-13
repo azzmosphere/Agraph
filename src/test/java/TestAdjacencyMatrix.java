b6e6659a5806b0fde3d89a41f88a36f0e787e94f
Aaron Spiteri <aaron.spiteri@staff.iinet.net.au>
2016-05-13 11:44:21 +1000

    Added UT's for matrix testing

--- src/test/java/TestAdjacencyMatrix.java
+++ src/test/java/TestAdjacencyMatrix.java
@@ -2,13 +2,26 @@ import azzmosphere.agraph.datastructures.UltraSonicSensor;
 import azzmosphere.agraph.plane.PlannerGraph;
 import azzmosphere.agraph.vertices.Vertex;
 import org.junit.Test;
-
 import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * Created by aaron.spiteri on 12/05/2016.
+ *
+ * Test Adjacency matrix creation and manipulation.
  */
 public class TestAdjacencyMatrix {
+
+    /*
+     * The graph that this is testing should be something like this:
+     *     1  2  3  4
+     *  1 v1-------v4
+     *  2  |       |
+     *  3  |       |
+     *  4 V2-------v3
+     */
     @Test
     public void shouldCreateCorrectAdjencyMatrix() throws Exception {
         PlannerGraph pg = new PlannerGraph();
@@ -27,8 +40,37 @@ public class TestAdjacencyMatrix {
 
         ArrayList<ArrayList<Boolean>> adjacentMatrix = pg.getAdjacencyMatrix();
 
+        /*
+         * Adjacency matrix should look like this:
+         * [false, true, true, true]
+         * [true, false, true, false]
+         * [true, true, false, true]
+         * [true, false, true, false]
+         */
 
+        // V1
+        assertThat(adjacentMatrix.get(0).get(0), is(false));
+        assertThat(adjacentMatrix.get(0).get(1), is(true));
+        assertThat(adjacentMatrix.get(0).get(2), is(true));
+        assertThat(adjacentMatrix.get(0).get(3), is(true));
 
+        // V2
+        assertThat(adjacentMatrix.get(1).get(0), is(true));
+        assertThat(adjacentMatrix.get(1).get(1), is(false));
+        assertThat(adjacentMatrix.get(1).get(2), is(true));
+        assertThat(adjacentMatrix.get(1).get(3), is(false));
+
+        // V3
+        assertThat(adjacentMatrix.get(2).get(0), is(true));
+        assertThat(adjacentMatrix.get(2).get(1), is(true));
+        assertThat(adjacentMatrix.get(2).get(2), is(false));
+        assertThat(adjacentMatrix.get(2).get(3), is(true));
+
+        // V4
+        assertThat(adjacentMatrix.get(3).get(0), is(true));
+        assertThat(adjacentMatrix.get(3).get(1), is(false));
+        assertThat(adjacentMatrix.get(3).get(2), is(true));
+        assertThat(adjacentMatrix.get(3).get(3), is(false));
 
         for (ArrayList<Boolean> i : adjacentMatrix) {
             System.out.println(i);
